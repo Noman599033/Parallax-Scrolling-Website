898a383c6a69e26cce438fbf6d99e044b9fe4f75
Noman599033 <abdnoman55@gmail.com>
2024-02-11 16:14:30 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,29 @@
+const list = document.querySelectorAll('.list');
+
+function activeLink() {
+    list.forEach((item) =>
+    item.classList.remove('active'));
+    this.classList.add('active');
+}
+
+list.forEach((item) =>
+item.addEventListener('click', activeLink));
+
+let text = document.getElementById('text');
+let leaf = document.getElementById('leaf');
+let hill1 = document.getElementById('hill1');
+let hill4 = document.getElementById('hill4');
+let hill5 = document.getElementById('hill5');
+
+window.addEventListener('scroll', () => {
+    let value = window.scrollY;
+
+    text.style.marginTop = value * 2.5 + 'px';
+    leaf.style.top = value * -1.5 + 'px';
+    leaf.style.left = value * 1.5 + 'px';
+    hill5.style.left = value * 1.5 + 'px';
+    hill4.style.left = value * -1.5 + 'px';
+    hill1.style.top = value * -1.5 + 'px';
+})
+
+
